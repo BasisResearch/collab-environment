98cab3eda3c179a9d3b60b5693330d977c32ca6c
Dmitry Batenkov <dima.batenkov@gmail.com>
2025-07-31 17:11:26 -0400

    ignore tracking NB

--- scripts/test_notebooks.sh
+++ scripts/test_notebooks.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
 INCLUDED_NOTEBOOKS="docs/"
-EXCLUDED_NOTEBOOKS="docs/data/gcloud_example_download.ipynb"
+EXCLUDED_NOTEBOOKS="docs/data/gcloud_example_download.ipynb docs/ docs/tracking/full_pipeline.ipynb"
 
 CI=1 pytest --nbval-lax --dist loadscope -n auto $INCLUDED_NOTEBOOKS --ignore $EXCLUDED_NOTEBOOKS
