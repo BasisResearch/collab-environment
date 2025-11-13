c38823fd08b674b55773f2dc8e0b842c8e237cf2
TOC6 <tom@basis.ai>
2025-11-13 18:24:01 -0500

    Undid previous change to setup.sh

--- setup.sh
+++ setup.sh
@@ -7,7 +7,7 @@ submodule_dir="./submodules/hloc"
 
 # Install the package
 cd $base_dir
-uv pip install -e .
+pip install -e .
 
 if [ ! -d "$submodule_dir" ]; then
     git clone --branch master --recursive https://github.com/cvg/Hierarchical-Localization.git $submodule_dir
@@ -16,4 +16,4 @@ fi
 cd $submodule_dir
 # git checkout v1.4
 git submodule update --init --recursive
-uv pip install -e . --no-cache-dir
+pip install -e . --no-cache-dir
