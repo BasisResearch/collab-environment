948e370c8569d2253198b102ab4fce115a981b46
Dmitry Batenkov <dima.batenkov@gmail.com>
2026-02-12 16:04:41 -0500

    script to run the dashboard easily

---
+++ scripts/analysis_dashboard.sh
@@ -0,0 +1,5 @@
+#!/bin/bash
+
+source $(dirname $0)/deploy/config.sh
+
+panel serve collab_env/dashboard/spatial_analysis_app.py --dev --show --port 5008 --static-dirs dashboard-static=collab_env/dashboard/static
